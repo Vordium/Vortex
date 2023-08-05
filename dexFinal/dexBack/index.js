cea341d07eec77b7330ac0177efcf77883b0fe7c
VORDIUM <Vordium@gmail.com>
2023-08-05 18:59:11 +0400

    new

--- dexFinal/dexBack/index.js
+++ dexFinal/dexBack/index.js
@@ -3,29 +3,30 @@ const Moralis = require("moralis").default;
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const port = 3001;
+
+// Replace the development port (3001) with the production URL
+const port = "https://swap.vordium.com/";
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/tokenPrice", async (req, res) => {
 
-  const {query} = req;
+  const { query } = req;
 
   const responseOne = await Moralis.EvmApi.token.getTokenPrice({
     address: query.addressOne
-  })
+  });
 
   const responseTwo = await Moralis.EvmApi.token.getTokenPrice({
     address: query.addressTwo
-  })
+  });
 
   const usdPrices = {
     tokenOne: responseOne.raw.usdPrice,
     tokenTwo: responseTwo.raw.usdPrice,
-    ratio: responseOne.raw.usdPrice/responseTwo.raw.usdPrice
-  }
-  
+    ratio: responseOne.raw.usdPrice / responseTwo.raw.usdPrice
+  };
 
   return res.status(200).json(usdPrices);
 });
@@ -34,6 +35,6 @@ Moralis.start({
   apiKey: process.env.MORALIS_KEY,
 }).then(() => {
   app.listen(port, () => {
-    console.log(`Listening for API Calls`);
+    console.log(`Listening for API Calls on ${port}`);
   });
 });
