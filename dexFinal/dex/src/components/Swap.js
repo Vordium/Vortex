988c9000eae3e9e733d0d843bb2829a766a548e4
VORDIUM <Vordium@gmail.com>
2023-08-21 06:37:38 +0400

    price fetch

--- dexFinal/dex/src/components/Swap.js
+++ dexFinal/dex/src/components/Swap.js
@@ -85,7 +85,7 @@ function Swap(props) {
 
   async function fetchPrices(one, two){
 
-      const res = await axios.get(`https://swap.vordium.com/tokenPrice`, {
+      const res = await axios.get(`http://54.242.66.179:3001/tokenPrice`, {
         params: {addressOne: one, addressTwo: two}
       })
 
