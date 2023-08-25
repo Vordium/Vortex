989dab1ac183f86e6ee202bfbeb63fedb7902492
VORDIUM <Vordium@gmail.com>
2023-08-25 21:16:51 +0400

    fix1

--- dexFinal/dexBack/api/tokenPrice.js
+++ dexFinal/dexBack/api/tokenPrice.js
@@ -1,23 +1,32 @@
 const Moralis = require("moralis").default;
 
-const { MORALIS_KEY } = process.env; // You can directly access environment variables in Vercel
+const { MORALIS_KEY } = process.env;
 
 module.exports = async (req, res) => {
-  const { query } = req;
+  try {
+    const { query } = req;
 
-  const responseOne = await Moralis.EvmApi.token.getTokenPrice({
-    address: query.addressOne,
-  });
+    console.log("Received request with query:", query);
 
-  const responseTwo = await Moralis.EvmApi.token.getTokenPrice({
-    address: query.addressTwo,
-  });
+    const responseOne = await Moralis.EvmApi.token.getTokenPrice({
+      address: query.addressOne,
+    });
 
-  const usdPrices = {
-    tokenOne: responseOne.raw.usdPrice,
-    tokenTwo: responseTwo.raw.usdPrice,
-    ratio: responseOne.raw.usdPrice / responseTwo.raw.usdPrice,
-  };
+    const responseTwo = await Moralis.EvmApi.token.getTokenPrice({
+      address: query.addressTwo,
+    });
 
-  res.status(200).json(usdPrices);
+    const usdPrices = {
+      tokenOne: responseOne.raw.usdPrice,
+      tokenTwo: responseTwo.raw.usdPrice,
+      ratio: responseOne.raw.usdPrice / responseTwo.raw.usdPrice,
+    };
+
+    console.log("Sending response:", usdPrices);
+
+    res.status(200).json(usdPrices);
+  } catch (error) {
+    console.error("An error occurred:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
