ed475745cc72e7524c18ba261a5474e95a000d12
VORDIUM <Vordium@gmail.com>
2023-08-26 21:31:38 +0400

    fixx

--- dexFinal/dexBack/api/tokenPrice/index.js
+++ dexFinal/dexBack/api/tokenPrice/index.js
@@ -1,21 +1,23 @@
 const Moralis = require("moralis").default;
 
 module.exports = async (req, res) => {
-  const { addressOne, addressTwo } = req.query;
-
-  if (!addressOne || !addressTwo) {
-    return res.status(400).json({ error: "Both addresses are required" });
-  }
-
   try {
-    Moralis.initialize(process.env.MORALIS_KEY);
+    const { query } = req;
+
+    console.log("Received request with query:", query);
+
+    console.log("Moralis API Key:", process.env.MORALIS_KEY);
+
+    if (!query.addressOne || !query.addressTwo) {
+      return res.status(400).json({ error: "Both addresses are required" });
+    }
 
     const responseOne = await Moralis.EvmApi.token.getTokenPrice({
-      address: addressOne,
+      address: query.addressOne,
     });
 
     const responseTwo = await Moralis.EvmApi.token.getTokenPrice({
-      address: addressTwo,
+      address: query.addressTwo,
     });
 
     const usdPrices = {
@@ -24,11 +26,11 @@ module.exports = async (req, res) => {
       ratio: responseOne.raw.usdPrice / responseTwo.raw.usdPrice,
     };
 
+    console.log("Sending response:", usdPrices);
+
     res.status(200).json(usdPrices);
   } catch (error) {
     console.error("An error occurred:", error);
-    res
-      .status(500)
-      .json({ error: "Internal server error", detailedError: error.message });
+    res.status(500).json({ error: "Internal server error", detailedError: error.message });
   }
 };
