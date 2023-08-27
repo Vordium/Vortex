e418441aca3776e60f60fafcad1ec4a8d82a6219
VORDIUM <Vordium@gmail.com>
2023-08-27 15:02:58 +0400

    tokennow

--- dexFinal/dex/src/components/Tokens.js
+++ dexFinal/dex/src/components/Tokens.js
@@ -37,10 +37,10 @@ function Tokens() {
       <div style={rowStyle}>
         {tokenList.map(token => (
           <div key={token.id} style={cardStyle}>
-            <img src={token.logo} alt={`${token.name} Logo`} style={logoStyle} />
+            <img src={e.img} alt={`${e.name} Logo`} style={logoStyle} />
             <div style={infoStyle}>
-              <h3 style={{ fontSize: '16px' }}>{token.name}</h3>
-              <p>{token.address}</p>
+              <h3 style={{ fontSize: '16px' }}>{e.name}</h3>
+              <p>{e.address}</p>
             </div>
           </div>
         ))}
@@ -50,7 +50,7 @@ function Tokens() {
           <div className="tokenChoice" key={i}>
             <img src={e.img} alt={e.ticker} className="tokenLogo" />
             <div className="tokenChoiceNames">
-              <div className="tokenName">{e.name} <p>{token.address}</p> </div>
+              <div className="tokenName">{e.name} <p>{e.address}</p></div>
               <div className="tokenTicker">{e.ticker}</div>
               
             </div>
