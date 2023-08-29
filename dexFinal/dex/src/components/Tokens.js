4714b0d9e76e635b23e96a0caacc2dbf53901bc0
VORDIUM <Vordium@gmail.com>
2023-08-29 21:04:36 +0400

    fixed

--- dexFinal/dex/src/components/Tokens.js
+++ dexFinal/dex/src/components/Tokens.js
@@ -52,7 +52,7 @@ const rowStyle = {
 
 const searchStyle = {
   position: 'fixed',  // Add this line
-  top: '0', 
+  top: '150px', 
   width: '50%',
   padding: '10px',
   border: '1px solid #ccc',
