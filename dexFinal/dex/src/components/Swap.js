2d5e9cac5da72de78d0518547b230e3b197a15d3
VORDIUM <Vordium@gmail.com>
2023-09-07 02:20:29 +0400

    fix

--- dexFinal/dex/src/components/Swap.js
+++ dexFinal/dex/src/components/Swap.js
@@ -104,11 +104,11 @@ function Swap(props) {
 
   async function fetchDexSwap(){
 
-    const allowance = await axios.get(`https://api.1inch.io/v5.2/1/approve/allowance?tokenAddress=${tokenOne.address}&walletAddress=${address}`)
+    const allowance = await axios.get(`https://api.1inch.dev/swap/v5.2/1/approve/allowance?tokenAddress=${tokenOne.address}&walletAddress=${address}`)
   
     if(allowance.data.allowance === "0"){
 
-      const approve = await axios.get(`https://api.1inch.io/v5.2/1/approve/transaction?tokenAddress=${tokenOne.address}`)
+      const approve = await axios.get(`https://api.1inch.dev/swap/v5.2/1/approve/allowance?tokenAddress=${tokenOne.address}`)
 
       setTxDetails(approve.data);
       console.log("not approved")
