ff69e26e1b9dd3b0a2137545ef8d66a24b76d521
VORDIUM <Vordium@gmail.com>
2023-09-08 23:03:17 +0400

    server

--- dexFinal/dex/api/proxy.js
+++ dexFinal/dex/api/proxy.js
@@ -1,20 +1,36 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const express = require('express');
+const axios = require('axios');
 
-export default (req, res) => {
-  // Configure the proxy middleware
-  const proxy = createProxyMiddleware({
-    target: "https://api.1inch.dev",
-    changeOrigin: true,
-    // Optionally, you can set headers if needed
-    onProxyReq: (proxyReq) => {
-      // Add your API key in the request header if required
-      proxyReq.setHeader(
-        "Authorization",
-        `Bearer ${process.env.REACT_APP_1INCH_KEY}`
-      );
-    },
-  });
+const app = express();
+const port = process.env.PORT || 3001;
 
-  // Use the proxy middleware to handle the request
-  proxy(req, res);
-};
+// Enable CORS (Cross-Origin Resource Sharing)
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+// Define a route to proxy 1inch API requests
+app.get('/1inch-proxy', async (req, res) => {
+  try {
+    const { url } = req.query;
+
+    if (!url) {
+      return res.status(400).json({ error: 'Missing URL parameter' });
+    }
+
+    // Make a request to the 1inch API using the provided URL
+    const response = await axios.get(url);
+
+    // Send the response from 1inch back to the frontend
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error proxying 1inch API request:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Start the Express server
+app.listen(port, () => {
+  console.log(`Proxy server is running on port ${port}`);
+});
