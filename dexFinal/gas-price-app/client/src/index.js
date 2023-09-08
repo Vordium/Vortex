3079574063c68b27cc43299b30da4b3cc282e329
VORDIUM <Vordium@gmail.com>
2023-09-09 03:31:55 +0400

    cd

--- dexFinal/gas-price-app/client/src/index.js
+++ dexFinal/gas-price-app/client/src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import './index.css'; // You may have a CSS file to style your app
+import App from './App'; // Import your main React component (e.g., App.js)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
