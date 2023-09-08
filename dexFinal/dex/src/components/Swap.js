7b1864defa2fa04e99005da855935d08fbde0943
VORDIUM <104957254+Vordium@users.noreply.github.com>
2023-09-08 20:05:03 +0400

    Update Swap.js

--- dexFinal/dex/src/components/Swap.js
+++ dexFinal/dex/src/components/Swap.js
@@ -123,12 +123,12 @@ function Swap(props) {
 
     try {
       const allowance = await axios.get(
-        `https://api.1inch.dev/swap/v5.2/1/approve/allowance?tokenAddress=${tokenOne.address}&walletAddress=${address}`
+        `/api/swap/v5.2/1/approve/allowance?tokenAddress=${tokenOne.address}&walletAddress=${address}`
       );
 
       if (allowance.data.allowance === "0") {
         const approve = await axios.get(
-          `https://api.1inch.dev/swap/v5.2/1/approve/allowance?tokenAddress=${tokenOne.address}`
+          `/api/swap/v5.2/1/approve/allowance?tokenAddress=${tokenOne.address}`
         );
         setTxDetails(approve.data);
         console.log("not approved");
@@ -136,7 +136,7 @@ function Swap(props) {
       }
 
       const tx = await axios.get(
-        `https://api.1inch.dev/swap/v5.2/1/swap?fromTokenAddress=${tokenOne.address}&toTokenAddress=${tokenTwo.address}&amount=${tokenOneAmount.padEnd(
+        `/api/swap/v5.2/1/swap?fromTokenAddress=${tokenOne.address}&toTokenAddress=${tokenTwo.address}&amount=${tokenOneAmount.padEnd(
           tokenOne.decimals + tokenOneAmount.length,
           "0"
         )}&fromAddress=${address}&slippage=${slippage}`
