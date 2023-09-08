310101457d81f390a45b874a698b2b3c9d9a421c
VORDIUM <Vordium@gmail.com>
2023-09-09 00:25:41 +0400

    fixss

--- dexFinal/dex/api/proxy.js
+++ dexFinal/dex/api/proxy.js
@@ -1,20 +1,13 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-export default (req, res) => {
-  // Configure the proxy middleware
-  const proxy = createProxyMiddleware({
-    target: "https://api.1inch.dev",
+module.exports = (req, res) => {
+  const apiProxy = createProxyMiddleware({
+    target: 'https://api.1inch.dev',
     changeOrigin: true,
-    // Optionally, you can set headers if needed
-    onProxyReq: (proxyReq) => {
-      // Add your API key in the request header if required
-      proxyReq.setHeader(
-        "Authorization",
-        `Bearer ${process.env.REACT_APP_1INCH_KEY}`
-      );
+    pathRewrite: {
+      '^/api/proxy': '', // Rewrite the path as needed
     },
   });
 
-  // Use the proxy middleware to handle the request
-  proxy(req, res);
+  apiProxy(req, res);
 };
