6a994677456da000d1098b1561bc1f1eed01ef9d
VORDIUM <104957254+Vordium@users.noreply.github.com>
2023-09-08 13:19:03 +0400

    Update Swap.js

--- dexFinal/dex/src/components/Swap.js
+++ dexFinal/dex/src/components/Swap.js
@@ -85,7 +85,7 @@ function Swap(props) {
 
   async function fetchPrices(one, two){
 
-      const res = await axios.get(`https://api.vordium.com/api/tokenParice`, {
+      const res = await axios.get(`https://api.vordium.com/api/tokenPrice`, {
         params: {addressOne: one, addressTwo: two}
       })
 
