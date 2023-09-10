09028562353af29515f8f0cdf64718f74019bc34
VORDIUM <Vordium@gmail.com>
2023-09-11 02:40:49 +0400

    sace

--- dexFinal/1inch-backend/api/1inch/server.js
+++ dexFinal/1inch-backend/api/1inch/server.js
@@ -1,7 +1,9 @@
 const axios = require('axios');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
-const cors = require("cors"); // Import the cors middleware
-require("dotenv").config();
 const port = process.env.PORT || 3000;
 
 // Configure CORS to allow requests from your frontend origin
@@ -13,6 +15,7 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 app.use(express.json());
+
 // Middleware and route configuration goes here
 
 // Define a route that makes a request to the 1inch API with authorization headers
@@ -20,6 +23,8 @@ app.get('/api/1inch', async (req, res) => {
   try {
     // Retrieve the 1inch API key from the environment variable
     const apiKey = process.env.REACT_APP_1INCH_KEY;
+
+    console.log('API Key:', apiKey); // Add a console log to display the API key
 
     // Configure Axios with the authorization header
     const axiosConfig = {
@@ -29,16 +34,18 @@ app.get('/api/1inch', async (req, res) => {
     };
 
     // Make the request to the 1inch API
+    console.log('Sending request to 1inch API'); // Add a console log to indicate the request
     const response = await axios.get(
       'https://api.1inch.dev/swap/v5.2/',
       axiosConfig
     );
 
     // Handle the response and send it to the client
+    console.log('Received response from 1inch API'); // Add a console log for the response
     res.json(response.data);
   } catch (error) {
     // Handle errors and send an error response to the client
-    console.error(error);
+    console.error('Error:', error); // Add a console log for the error
     res.status(500).json({ error: 'Internal server error' });
   }
 });
