6b1deadff72f8fdf27d429e03a68910a3d89acb6
VORDIUM <Vordium@gmail.com>
2023-09-11 05:04:24 +0400

    sascs

--- dexFinal/dex/api/1inch/index.js
+++ dexFinal/dex/api/1inch/index.js
@@ -1,20 +1,36 @@
 const axios = require('axios');
 
-// ...
-
-app.get('/api/1inch/', async (req, res) => {
+module.exports = async (req, res) => {
   try {
-    const apiKey = process.env.REACT_APP_1INCH_KEY;
-    const axiosConfig = {
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-      },
+    // Define the 1inch API URL
+    const apiUrl = 'https://api.1inch.dev/swap/v5.2/';
+
+    // Retrieve the 1inch API key from the Vercel environment variable
+    const apiKey = process.env['1INCH_API_KEY'];
+
+    if (!apiKey) {
+      throw new Error('1INCH_API_KEY environment variable not found.');
+    }
+
+    // Configure headers for the request to the 1inch API
+    const headers = {
+      Accept: 'application/json',
+      Authorization: `Bearer ${apiKey}`,
     };
-    const apiUrl = `https://api.1inch.dev/swap/v5.2/`;
-    const response = await axios.get(apiUrl, axiosConfig);
-    res.json(response.data);
+
+    // Make a GET request to the 1inch API with the specified headers
+    const response = await axios.get(apiUrl, { headers });
+
+    // Check if the response is "Unauthorized" (status code 401)
+    if (response.status === 401) {
+      res.status(401).json({ success: false, error: 'Unauthorized' });
+    } else {
+      // Forward the response from the 1inch API to the client for other status codes
+      res.status(response.status).json(response.data);
+    }
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
+    // Handle other errors and send an error response to the client
+    console.error('Error:', error);
+    res.status(500).json({ success: false, error: 'Internal server error' });
   }
-});
+};
