2fbb9ad7f1bed51461f3f4bc928707e206f9527c
VORDIUM <Vordium@gmail.com>
2023-09-11 04:58:37 +0400

    figds

--- dexFinal/1inch-backend/api/1inch/index.js
+++ dexFinal/1inch-backend/api/1inch/index.js
@@ -5,8 +5,21 @@ module.exports = async (req, res) => {
     // Define the 1inch API URL
     const apiUrl = 'https://api.1inch.dev/swap/v5.2/';
 
-    // Make a request to the 1inch API
-    const response = await axios.get(apiUrl);
+    // Retrieve the 1inch API key from the Vercel environment variable
+    const apiKey = process.env['1INCH_API_KEY'];
+
+    if (!apiKey) {
+      throw new Error('1INCH_API_KEY environment variable not found.');
+    }
+
+    // Configure headers for the request to the 1inch API
+    const headers = {
+      Accept: 'application/json',
+      Authorization: `Bearer ${apiKey}`,
+    };
+
+    // Make a GET request to the 1inch API with the specified headers
+    const response = await axios.get(apiUrl, { headers });
 
     // Forward the response from the 1inch API to the client
     res.status(response.status).json(response.data);
