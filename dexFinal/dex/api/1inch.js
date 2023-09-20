1858eae9a095db9adb19bfce5ab4a9a519b1ab9e
VORDIUM <Vordium@gmail.com>
2023-09-20 22:55:43 +0400

    add api

--- dexFinal/dex/api/1inch.js
+++ dexFinal/dex/api/1inch.js
@@ -3,7 +3,21 @@
 export default async (req, res) => {
     try {
       const { url } = req.query;
-      const response = await fetch(url); // Use the built-in fetch function
+  
+      // Define the headers with your API key
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: "Bearer REACT_APP_1INCH_KEY",
+      };
+  
+      // Make the fetch request with the headers
+      const response = await fetch(url, { headers });
+  
+      if (!response.ok) {
+        // Handle non-OK responses if needed
+        throw new Error(`Request failed with status ${response.status}`);
+      }
+  
       const data = await response.json();
       res.status(200).json(data);
     } catch (error) {
