49e39bfb39e037a22a1ed97cfb6e19f837841ec4
VORDIUM <Vordium@gmail.com>
2023-09-22 23:13:52 +0400

    check uppercase

--- dexFinal/dex/src/components/Egas.js
+++ dexFinal/dex/src/components/Egas.js
@@ -1,22 +1,25 @@
-import React from 'react'; // You need to import React
+import React from 'react';
 import { MdLocalGasStation } from 'react-icons/md';
 import { useFeeData } from 'wagmi';
-import { ethers } from 'ethers'; // Import ethers from 'ethers'
+import { ethers } from 'ethers';
 
 export const Egas = ({ size, className, units }) => {
     const { data, isError, isLoading } = useFeeData();
 
     if (isError || isLoading) return <></>;
-    const valueInWei = ethers.utils.formatUnits(data?.formatted?.gasPrice || '0', 9); // Corrected the usage of formatUnits
+
+    // Capitalize the value by using toUpperCase()
+    const valueInWei = ethers.utils.formatUnits(data?.formatted?.gasPrice || '0', 9).toUpperCase();
+
     return (
         <div className="flex flex-col mt-1 cursor-default">
             <div className={`flex items-center gap-0.5 leading-3 ${className || ''}`}>
                 <MdLocalGasStation size={size || ''} />
                 <span className="">
-                    {Math.round(Number(valueInWei))} {/* Use valueInWei instead of formatUnits */}
+                    {Math.round(Number(valueInWei))}
                 </span>
             </div>
-            {units && <span className="text-[10px] text-neutral-400 leading-3">{units}</span>}
+            {units && <span className="text-[7px] text-neutral-400 leading-3">{units.toUpperCase()}</span>}
         </div>
     );
 };
