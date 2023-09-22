18f91f045d64fae1571cb0fd39b51e1d6ccfe9df
VORDIUM <Vordium@gmail.com>
2023-09-22 23:04:59 +0400

    fixss

--- dexFinal/dex/src/components/Egas.js
+++ dexFinal/dex/src/components/Egas.js
@@ -1,24 +1,19 @@
+import React from 'react'; // You need to import React
 import { MdLocalGasStation } from 'react-icons/md';
 import { useFeeData } from 'wagmi';
-import { ethers } from 'ethers';
+import { ethers } from 'ethers'; // Import ethers from 'ethers'
 
-//type IEgas = {
-//    size?: number;
-//    className?: string;
-//    units?: 'gwei';
-//};
-
-const valueInWei = ethers.utils.formatUnits(valueInWei, 9);
 export const Egas = ({ size, className, units }) => {
     const { data, isError, isLoading } = useFeeData();
 
     if (isError || isLoading) return <></>;
+    const valueInWei = ethers.utils.formatUnits(data?.formatted?.gasPrice || '0', 9); // Corrected the usage of formatUnits
     return (
         <div className="flex flex-col mt-1 cursor-default">
             <div className={`flex items-center gap-0.5 leading-3 ${className || ''}`}>
                 <MdLocalGasStation size={size || ''} />
                 <span className="">
-                    {Math.round(Number(formatUnits(data?.formatted?.gasPrice || '0', 9)))}
+                    {Math.round(Number(valueInWei))} {/* Use valueInWei instead of formatUnits */}
                 </span>
             </div>
             {units && <span className="text-[10px] text-neutral-400 leading-3">{units}</span>}
