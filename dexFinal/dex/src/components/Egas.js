9b311d52d8ece74708ed277f884977c8934c0e69
VORDIUM <Vordium@gmail.com>
2023-09-22 23:47:46 +0400

    change icon size

--- dexFinal/dex/src/components/Egas.js
+++ dexFinal/dex/src/components/Egas.js
@@ -3,7 +3,7 @@ import { MdLocalGasStation } from 'react-icons/md';
 import { useFeeData } from 'wagmi';
 import { ethers } from 'ethers';
 
-export const Egas = ({ size, className, units }) => {
+export const Egas = ({ iconSize, className, units }) => { // Renamed size prop to iconSize
     const { data, isError, isLoading } = useFeeData();
 
     if (isError || isLoading) return <></>;
@@ -14,7 +14,7 @@ export const Egas = ({ size, className, units }) => {
     return (
         <div className="flex flex-col mt-1 cursor-default">
             <div className={`flex items-center gap-0.2 leading-3 ${className || ''}`}>
-                <MdLocalGasStation size={size || ''} />
+                <MdLocalGasStation size={iconSize || 18} /> {/* Set a default size (e.g., 24) */}
                 <span style={{ fontSize: '12px' }}>
                     {Math.round(Number(valueInWei))}
                 </span>
