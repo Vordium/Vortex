418e8a50029872c796bba0aeca3274aa478c9b5d
VORDIUM <Vordium@gmail.com>
2023-09-23 02:06:11 +0400

    teedec

--- dexFinal/dex/src/components/Swap.js
+++ dexFinal/dex/src/components/Swap.js
@@ -247,44 +247,47 @@ function Swap(props) {
         </div>
       </Modal>
       <div className="tradeBox">
-        <div className="tradeBoxHeader">
-          <h4>Swap</h4>
-          <Popover
-            content={settings}
-            title="Settings"
-            trigger="click"
-            placement="bottomRight"
-          >
-            <SettingOutlined className="cog" />
-          </Popover>
-        </div>
-        <div className="inputs">
-          <Input
+      <div className="tradeBoxHeader">
+        <h4>Swap</h4>
+        <Popover
+          content={settings}
+          title="Settings"
+          trigger="click"
+          placement="bottomRight"
+        >
+          <SettingOutlined className="cog" />
+        </Popover>
+      </div>
+      <div className="inputs">
+        <Input.Group compact>
+          <InputNumber
+            style={{ width: '70%' }}
             placeholder="0"
             value={tokenOneAmount}
-            onChange={changeAmount}       
+            onChange={changeAmount}
             disabled={!prices}
-            addonAfter={ // Add this addonAfter property
-              <div>
-                Balance: {data?.formatted} {data?.symbol}
-              </div>
-            }
           />
-          <Input placeholder="0" value={tokenTwoAmount} disabled={true} />
-          <div className="switchButton" onClick={switchTokens}>
-            <ArrowDownOutlined className="switchArrow" />
-          </div>
-          <div className="assetOne" onClick={() => openModal(1)}>
-            <img src={tokenOne.img} alt="assetOneLogo" className="assetLogo" />
-            {tokenOne.ticker}
-            <DownOutlined />
-          </div>
-          <div className="assetTwo" onClick={() => openModal(2)}>
-            <img src={tokenTwo.img} alt="assetOneLogo" className="assetLogo" />
-            {tokenTwo.ticker}
-            <DownOutlined />
-          </div>
+          <Input
+            style={{ width: '30%', textAlign: 'left' }}
+            placeholder="Balance: 1234.56 ETH"
+            disabled
+          />
+        </Input.Group>
+        <Input placeholder="0" value={tokenTwoAmount} disabled={true} />
+        <div className="switchButton" onClick={switchTokens}>
+          <ArrowDownOutlined className="switchArrow" />
         </div>
+        <div className="assetOne" onClick={() => openModal(1)}>
+          <img src={tokenOne.img} alt="assetOneLogo" className="assetLogo" />
+          {tokenOne.ticker}
+          <DownOutlined />
+        </div>
+        <div className="assetTwo" onClick={() => openModal(2)}>
+          <img src={tokenTwo.img} alt="assetOneLogo" className="assetLogo" />
+          {tokenTwo.ticker}
+          <DownOutlined />
+        </div>
+      </div>
         <div
           className="swapButton"
           disabled={!tokenOneAmount || !isConnected || isLoading}
@@ -295,7 +298,7 @@ function Swap(props) {
         <div className="gas-container">
         {/* Use the Egas component with alignment */}
         <Egas size={16} className="my-gas-icon" units="gwei" alignRight />
-    </div>
+         </div>
         <div className="expandableContainer">
           <div className="expandableBox" onClick={toggleBoxExpansion}>
             <div className="expandableBoxHeader">
