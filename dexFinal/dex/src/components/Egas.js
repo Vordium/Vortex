19da3e6ce8ff4e7b94ed35c179c81127f99c408f
VORDIUM <Vordium@gmail.com>
2023-09-25 03:36:59 +0400

    files

--- dexFinal/dex/src/components/Egas.js
+++ dexFinal/dex/src/components/Egas.js
@@ -1,38 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { MdLocalGasStation } from 'react-icons/md';
 import { useFeeData } from 'wagmi';
 import { ethers } from 'ethers';
 
 const rowStyle = {
-  display: 'flex',
-  flexDirection: 'row',
-  alignItems: 'center',
-  gap: '0.2rem',
-  lineHeight: '1.5',
-  justifyContent: 'flex-end',
+    display: 'flex',
+    flexDirection: 'row', // Set the flex direction to "row"
+    alignItems: 'center',
+    gap: '0.2rem',
+    lineHeight: '1.5',
+    justifyContent: 'flex-end',
 };
 
 export const Egas = ({ iconSize, className, units }) => {
-  const { data, isError, isLoading } = useFeeData();
-  const [gasPrice, setGasPrice] = useState(null);
+    const { data, isError, isLoading } = useFeeData();
 
-  useEffect(() => {
-    if (!isError && !isLoading && data && data.formatted && data.formatted.gasPrice) {
-      const gasPriceWei = ethers.BigNumber.from(data.formatted.gasPrice);
-      const gasPriceEth = ethers.utils.formatEther(gasPriceWei);
-      setGasPrice(parseFloat(gasPriceEth).toFixed(2));
-    }
-  }, [data, isError, isLoading]);
+    if (isError || isLoading) return <></>;
 
-  if (isError || isLoading || gasPrice === null) return null;
+    // Convert the gas price to a float with 2 decimal places
+    const gasPriceInWei = parseFloat(ethers.utils.formatUnits(data?.formatted?.gasPrice || '0', 9)).toFixed(2);
 
-  const gasValueWithSymbol = `~$${gasPrice}`;
+    // Add "~$" to the left of the value
+    const gasValueWithSymbol = `~$${gasPriceInWei}`;
 
-  return (
-    <div style={rowStyle} className={className || ''}>
-      <MdLocalGasStation size={iconSize || 16} />
-      <span style={{ fontSize: '12px' }}>{gasValueWithSymbol}</span>
-      {units && <span style={{ fontSize: '12px' }}>{units.toUpperCase()}</span>}
-    </div>
-  );
+    return (
+        <div style={rowStyle} className={className || ''}>
+            <MdLocalGasStation size={16} /> {/* Change the icon size to 16px */}
+            <span style={{ fontSize: '12px' }}>
+                {gasValueWithSymbol}
+            </span>
+            {units && <span style={{ fontSize: '12px' }}>{units.toUpperCase()}</span>}
+        </div>
+    );
 };
