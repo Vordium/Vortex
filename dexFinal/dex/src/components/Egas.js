bbc4ea870f3c8e4b68b30a31d4eeabfae70e2001
VORDIUM <Vordium@gmail.com>
2023-09-24 22:09:56 +0400

    fix

--- dexFinal/dex/src/components/Egas.js
+++ dexFinal/dex/src/components/Egas.js
@@ -1,35 +1,43 @@
 import React from 'react';
 import { MdLocalGasStation } from 'react-icons/md';
 import { useFeeData } from 'wagmi';
-import { ethers } from 'ethers';
+import { ethers, BigNumber } from 'ethers'; // Import BigNumber from ethers
 
 const rowStyle = {
-    display: 'flex',
-    flexDirection: 'row', // Set the flex direction to "row"
-    alignItems: 'center',
-    gap: '0.2rem',
-    lineHeight: '1.5',
-    justifyContent: 'flex-end',
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  gap: '0.2rem',
+  lineHeight: '1.5',
+  justifyContent: 'flex-end',
 };
 
-export const Egas = ({ iconSize, className, units }) => {
-    const { data, isError, isLoading } = useFeeData();
+const Egas = ({ iconSize = 16, className = '', units = '' }) => {
+  const { data, isError, isLoading } = useFeeData();
 
-    if (isError || isLoading) return <></>;
+  if (isError || isLoading || !data || !data.formatted || !data.formatted.gasPrice) {
+    return null; // Handle missing or invalid data
+  }
 
-    // Convert the gas price to a float with 2 decimal places
-    const gasPriceInWei = parseFloat(ethers.utils.formatUnits(data?.formatted?.gasPrice || '0', 9)).toFixed(2);
+  // Attempt to convert gasPrice to a BigNumber
+  let gasPriceInWei;
+  try {
+    gasPriceInWei = BigNumber.from(data.formatted.gasPrice); // Use BigNumber.from to convert
+  } catch (error) {
+    console.error('Error converting gasPrice to BigNumber:', error);
+    return null; // Handle the conversion error gracefully
+  }
 
-    // Add "~$" to the left of the value
-    const gasValueWithSymbol = `~$${gasPriceInWei}`;
+  // Add "~$" to the left of the value
+  const gasValueWithSymbol = `~$${gasPriceInWei.toNumber().toFixed(2)}`;
 
-    return (
-        <div style={rowStyle} className={className || ''}>
-            <MdLocalGasStation size={16} /> {/* Change the icon size to 16px */}
-            <span style={{ fontSize: '12px' }}>
-                {gasValueWithSymbol}
-            </span>
-            {units && <span style={{ fontSize: '12px' }}>{units.toUpperCase()}</span>}
-        </div>
-    );
+  return (
+    <div style={rowStyle} className={className}>
+      <MdLocalGasStation size={iconSize} />
+      <span style={{ fontSize: '12px' }}>{gasValueWithSymbol}</span>
+      {units && <span style={{ fontSize: '12px' }}>{units.toUpperCase()}</span>}
+    </div>
+  );
 };
+
+export default Egas;
