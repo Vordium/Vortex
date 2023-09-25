dcb43000bf4214a43d8a4edeaf4b0f22b0ac5ef2
VORDIUM <104957254+Vordium@users.noreply.github.com>
2023-09-25 13:30:52 +0400

    Update Egas try

--- dexFinal/dex/src/components/Egas.js
+++ dexFinal/dex/src/components/Egas.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { MdLocalGasStation } from 'react-icons/md';
 import { useFeeData } from 'wagmi';
-import { isBytes } from 'viem';
 
 const rowStyle = {
     display: 'flex',
@@ -14,19 +13,12 @@ const rowStyle = {
 
 export const Egas = ({ iconSize, className, units }) => {
     const { data, isError, isLoading } = useFeeData();
-    const gasPriceString = data?.formatted?.gasPrice || '0';
-
-    // Check if gasPriceString is a valid BigInt string using viem's isBytes
-    const isGasPriceValid = isBytes(gasPriceString);
-
-    // Provide a default value (0) if gasPriceString is not valid
-    const gasPriceInWei = isGasPriceValid
-        ? (parseFloat(gasPriceString) / 1e9).toFixed(2)
-        : 0;
-
-    const gasValueWithSymbol = `~$${gasPriceInWei}`;
 
     if (isError || isLoading) return <></>;
+
+    // Convert the gas price to a float with 2 decimal places using native BigInt
+    const gasPriceInWei = (BigInt(data?.formatted?.gasPrice || '0') / BigInt(10 ** 9)).toString();
+    const gasValueWithSymbol = `~$${(gasPriceInWei / 100).toFixed(2)}`;
 
     return (
         <div style={rowStyle} className={className || ''}>
