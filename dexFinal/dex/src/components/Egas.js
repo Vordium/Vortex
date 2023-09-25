dd3a0cdfed4b5cb014c9d1374817f9dc5f995a82
VORDIUM <104957254+Vordium@users.noreply.github.com>
2023-09-25 12:43:32 +0400

    Update Egas.js

--- dexFinal/dex/src/components/Egas.js
+++ dexFinal/dex/src/components/Egas.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MdLocalGasStation } from 'react-icons/md';
 import { useFeeData } from 'wagmi';
-import { ethers } from 'ethers';
+import { isBytes } from 'viem';
 
 const rowStyle = {
     display: 'flex',
@@ -16,12 +16,12 @@ export const Egas = ({ iconSize, className, units }) => {
     const { data, isError, isLoading } = useFeeData();
     const gasPriceString = data?.formatted?.gasPrice || '0';
 
-    // Check if gasPriceString is a valid BigNumber string
-    const isGasPriceValid = ethers.BigNumber.isBigNumber(gasPriceString);
+    // Check if gasPriceString is a valid BigInt string using viem's isBytes
+    const isGasPriceValid = isBytes(gasPriceString);
 
     // Provide a default value (0) if gasPriceString is not valid
     const gasPriceInWei = isGasPriceValid
-        ? parseFloat(ethers.utils.formatUnits(gasPriceString, 9)).toFixed(2)
+        ? (parseFloat(gasPriceString) / 1e9).toFixed(2)
         : 0;
 
     const gasValueWithSymbol = `~$${gasPriceInWei}`;
