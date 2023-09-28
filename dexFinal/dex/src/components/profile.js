da94c5ea2fa28ab212e0bee7961dfb98752eab89
VORDIUM <Vordium@gmail.com>
2023-09-28 23:51:53 +0400

    no svg

--- dexFinal/dex/src/components/profile.js
+++ dexFinal/dex/src/components/profile.js
@@ -7,7 +7,7 @@ import {
     useEnsAvatar,
     useEnsName,
   } from 'wagmi';
-  import metamaskLogo from '../assets/metamask-logo.svg'; // Replace with the actual image path
+   // Replace with the actual image path
   import coinbaseWalletLogo from '../assets/coinbase-wallet-logo.png'; // Replace with the actual image path
   import walletConnectLogo from '../assets/wallet-connect-logo.png'; // Replace with the actual image path
   import injectedConnectorLogo from '../assets/injected-connector-logo.png'; // Replace with the actual image path
