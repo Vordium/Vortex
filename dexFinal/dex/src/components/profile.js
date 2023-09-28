2b019483ee37eede6879ac53ee030dad39700eae
VORDIUM <Vordium@gmail.com>
2023-09-29 01:15:48 +0400

    fix thos

--- dexFinal/dex/src/components/profile.js
+++ dexFinal/dex/src/components/profile.js
@@ -1,79 +1,85 @@
 import './styles.css';
 import fallbackAvatar from '../assets/pastel.png';
 import {
-    useAccount,
-    useConnect,
-    useDisconnect,
-    useEnsAvatar,
-    useEnsName,
-  } from 'wagmi';
-  import metamaskLogo from '../assets/metamask-logo.png'; // Replace with the actual image path
-  import coinbaseWalletLogo from '../assets/coinbase-wallet-logo.png'; // Replace with the actual image path
-  import walletConnectLogo from '../assets/wallet-connect-logo.png'; // Replace with the actual image path
-  import injectedConnectorLogo from '../assets/injected-connector-logo.png'; // Replace with the actual image path
-  
-  export function Profile() {
-    const { address, connector, isConnected } = useAccount()
-    const { data: ensAvatar } = useEnsAvatar({ address })
-    const { data: ensName } = useEnsName({ address })
-    const { connect, connectors, error, isLoading, pendingConnector } =
-      useConnect()
-    const { disconnect } = useDisconnect()
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useEnsAvatar,
+  useEnsName,
+} from 'wagmi';
+import metamaskLogo from '../assets/metamask-logo.png'; // Replace with the actual image path
+import coinbaseWalletLogo from '../assets/coinbase-wallet-logo.png'; // Replace with the actual image path
+import walletConnectLogo from '../assets/wallet-connect-logo.png'; // Replace with the actual image path
+import injectedConnectorLogo from '../assets/injected-connector-logo.png'; // Replace with the actual image path
 
-    const connectorImages = {
-        MetaMask: metamaskLogo,
-        Coinbase_Wallet: coinbaseWalletLogo,
-        WalletConnect: walletConnectLogo,
-        Injected: injectedConnectorLogo,
-      };
-  
-    if (isConnected) {
-        const addressToShow = `${address.substring(0, 3)}...${address.substring(address.length - 3)}`;
-    
-        return (
-          <div className="container">
-            <div className="avatar">
-              <img src={ensAvatar || fallbackAvatar} alt="ENS Avatar" className="avatar-img" />
-            </div>
-            <div className="address">
-              <div className="name">
-                {ensName ? `${ensName}` : 'NoEns'}
-              </div>
-              <div className="address-text">
-                {addressToShow}
-              </div>
-            </div>
-            <div className="disconnect">
-              <button onClick={disconnect} className="disconnect-button">
-                Disconnect
-              </button>
-            </div>
-          </div>
-        )
-      }
-  
+export function Profile() {
+  const { address, connector, isConnected } = useAccount();
+  const { data: ensAvatar } = useEnsAvatar({ address });
+  const { data: ensName } = useEnsName({ address });
+  const { connect, connectors, error, isLoading, pendingConnector } = useConnect();
+  const { disconnect } = useDisconnect();
+
+  const connectorImages = {
+    MetaMask: metamaskLogo,
+    CoinbaseWallet: coinbaseWalletLogo,
+    WalletConnect: walletConnectLogo,
+    Injected: injectedConnectorLogo,
+    defaultConnectorImage: coinbaseWalletLogo,
+  };
+
+  if (isConnected) {
+    const addressToShow = `${address.substring(0, 3)}...${address.substring(address.length - 3)}`;
+
     return (
-        <div>
-    {connectors.map((connector) => (
-  <button
-    className="connect-button" // Apply the connect-button class
-    disabled={!connector.ready}
-    key={connector.id}
-    onClick={() => connect({ connector })}
-  >
-    <div className="connector-content">
-      <img src={connectorImages[connector.name]} alt={connector.name} className="connector-img" />
-      <span className="connector-name">{connector.name}</span>
-    </div>
-    {!connector.ready && ' (unsupported)'}
-    {isLoading &&
-      connector.id === pendingConnector?.id &&
-      ' (connecting)'}
-  </button>
-))}
-
-    {error && <div className="error">{error.message}</div>}
-  </div>
+      <div className="container">
+        <div className="avatar">
+          <img src={ensAvatar || fallbackAvatar} alt="ENS Avatar" className="avatar-img" />
+        </div>
+        <div className="address">
+          <div className="name">
+            {ensName ? `${ensName}` : 'NoEns'}
+          </div>
+          <div className="address-text">
+            {addressToShow}
+          </div>
+        </div>
+        <div className="disconnect">
+          <button onClick={disconnect} className="disconnect-button">
+            Disconnect
+          </button>
+        </div>
+      </div>
     )
   }
-  
+
+  return (
+    <div>
+      {connectors.map((connector) => (
+        <button
+          className="connect-button" // Apply the connect-button class
+          disabled={!connector.ready}
+          key={connector.id}
+          onClick={() => connect({ connector })}
+        >
+          <div className="connector-content">
+            <img
+              src={connectorImages[connector.name] || defaultConnectorImage}
+              alt={connector.name}
+              className="connector-img"
+              onError={(e) => {
+                e.target.src = defaultConnectorImage; // Replace with the path to your fallback image
+                e.target.alt = 'Fallback Connector Image'; // Replace with appropriate alt text
+              }}
+            />
+            <span className="connector-name">{connector.name || 'Unknown Connector'}</span>
+          </div>
+          {!connector.ready && ' (unsupported)'}
+          {isLoading &&
+            connector.id === pendingConnector?.id &&
+            ' (connecting)'}
+        </button>
+      ))}
+      {error && <div className="error">{error.message}</div>}
+    </div>
+  )
+}
