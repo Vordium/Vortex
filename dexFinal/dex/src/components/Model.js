7d6e2da5dc0ad12bfc039cdaa34065c900fb9bdd
VORDIUM <Vordium@gmail.com>
2023-10-03 01:10:20 +0400

    fix

--- dexFinal/dex/src/components/Model.js
+++ dexFinal/dex/src/components/Model.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Model.css'; // Import the CSS file for your modal styling
-import  {Profile} from "../profile";
+import  {Profile} from "./profile";
 function Modal({ onClose }) {
   return (
     <div className="modal-overlay" onClick={onClose}>
