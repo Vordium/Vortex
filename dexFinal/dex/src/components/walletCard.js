bf17a58ad1dba4c6d1f925ac866fd371ce3bb486
VORDIUM <Vordium@gmail.com>
2023-10-04 00:34:45 +0400

    close

--- dexFinal/dex/src/components/walletCard.js
+++ dexFinal/dex/src/components/walletCard.js
@@ -1,21 +1,23 @@
-import './Model.css'; // Import the CSS file for your modal styling
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
-
-
+import './Model.css'; // Import the CSS file for your modal styling
 
 function WalletCard({ onClose }) {
+  const handleClose = () => {
+    onClose(); // Call the onClose function passed as a prop
+  };
+
   return (
     <div className="card-modal-overlay" onClick={onClose}>
       <div className="card-modal" onClick={(e) => e.stopPropagation()}>
         <div>
-        <div className="account-card-header">
-          <h4>Account</h4>
-          <button className="close-button" onClick={(e) => e.stopPropagation()}>
-                <FontAwesomeIcon icon={faTimes} />
+          <div className="account-card-header">
+            <h4>Account</h4>
+            <button className="close-button" onClick={handleClose}>
+              <FontAwesomeIcon icon={faTimes} />
             </button>
-        </div>
+          </div>
           {/* Additional components or logic */}
         </div>
       </div>
